77263f589cac6d5384c9a779faa40c637edbaf45
Perminder <perminder_suman@yahoo.com>
2023-08-23 09:19:30 +0100

    End of 253: Creating an angular production build

--- API/Program.cs
+++ API/Program.cs
@@ -28,9 +28,14 @@ app.UseCors(builder => builder
     .WithOrigins("https://localhost:4200"));
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 app.MapControllers();
 app.MapHub<PresenceHub>("hubs/presence");
 app.MapHub<MessageHub>("hubs/message");
+app.MapFallbackToController("Index", "Fallback");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
