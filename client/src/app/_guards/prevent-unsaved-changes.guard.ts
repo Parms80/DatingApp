e6d457a43a78489b0cedd41fc18f71cbdf943b2b
Perminder <perminder_suman@yahoo.com>
2023-08-25 13:38:46 +0100

    Removed git file from client folder

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,14 @@
+import { Injectable, inject } from '@angular/core';
+import { CanDeactivate, CanDeactivateFn } from '@angular/router';
+import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { ConfirmService } from '../_services/confirm.service';
+
+export const PreventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> = (component) => {
+  const confirmService = inject(ConfirmService);
+  
+  if (component.editForm?.dirty) {
+    return confirmService.confirm();
+  }
+  
+  return true;
+};
---
+++ client/src/app/messages/messages.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Message } from '../_models/message';
+import { Pagination } from '../_models/pagination';
+import { MessageService } from '../_services/message.service';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+  messages?: Message[];
+  pagination?: Pagination;
+  container = 'Unread';
+  pageNumber = 1;
+  pageSize = 5;
+  loading = false;
+
+  constructor(private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.loading = true;
+    const fetchMessages$ = this.messageService.getMessages(this.pageNumber, this.pageSize, this.container);
+
+    fetchMessages$.subscribe({
+      next: response => {
+        this.messages = response.result;
+        this.pagination = response.pagination;
+        this.loading = false;
+      }
+    })
+  }
+
+  deleteMessage(id: number) {
+    const deleteMessage$ = this.messageService.deleteMessage(id);
+    
+    deleteMessage$.subscribe({
+      next: () => {
+        if (this.messages) {
+          const indexToDelete = this.messages.findIndex(m => m.id === id);
+
+          if (indexToDelete !== -1) {
+            this.messages?.splice(indexToDelete, 1)
+          }
+        }
+      }
+    })
+  }
+
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMessages();
+    }
+  }
+}
